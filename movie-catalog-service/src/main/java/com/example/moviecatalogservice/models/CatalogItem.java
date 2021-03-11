dc3070bcccaf325820bf78853cd3c7b2bdf733b2
YousF9 <theyous@gmail.com>
2021-03-11 12:43:49 -0600

    Initialize Movie Service API

---
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/models/CatalogItem.java
@@ -0,0 +1,38 @@
+package com.example.moviecatalogservice.models;
+
+public class CatalogItem {
+
+    private String name;
+    private String desc;
+    private int rating;
+
+    public CatalogItem(final String name, final String desc, final int rating) {
+        this.name = name;
+        this.desc = desc;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(final String desc) {
+        this.desc = desc;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(final int rating) {
+        this.rating = rating;
+    }
+}
